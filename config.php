4b059735c5a8da1f02941c7d44d3fe9d437ccc53
ritika-abc <ritikasss321@gmail.com>
2025-05-21 12:27:22 +0530

    load

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","u496524825_user_box","Vistarauserbox123","u496524825_vistara_enterp");
-// live
-// $con = mysql_connect("localhost","root","","vistara_enterprises");
-
-?>
--- vistara_enterprises/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","vistara_enterprises");
-// live
-// $con = mysql_connect("localhost","root","","vistara_enterprises");
-
-?>
